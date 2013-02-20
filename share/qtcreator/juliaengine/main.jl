ef031743343fa74fab48416e63ca0934a54a6acb
Westley <WestleyArgentum@gmail.com>
2013-02-20 09:25:55 -0800

    re-order initialization of network module so that packages have been set up before hand

--- share/qtcreator/juliaengine/main.jl
+++ share/qtcreator/juliaengine/main.jl
@@ -5,13 +5,10 @@ require("consolelogic.jl")
 require("sandbox.jl")
 
 __Sandbox.AddModule(__Event)
-__Sandbox.AddModule(__Network)
 __Sandbox.AddModule(__ConsoleLogic)
+__Sandbox.AddModule(__Network)
 
 __Sandbox.Init()
 
-#Event.NewEvent(Sandbox.GetSystem(Event), "network-output", "boo", 1, 2, 3)
-
-#println("running!!!")
 __Sandbox.Run( __Event.EventLoop )
 
