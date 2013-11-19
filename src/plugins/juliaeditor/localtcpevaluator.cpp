0f559420ad3b1c94bb6b957b9c503f8f8b1e53b8
keesvp <keesvp@gmail.com>
2013-11-19 10:04:27 -0800

    add argument -F to force reading .juliarc.jl. fix #165

--- src/plugins/juliaeditor/localtcpevaluator.cpp
+++ src/plugins/juliaeditor/localtcpevaluator.cpp
@@ -329,7 +329,8 @@ void LocalTcpEvaluator::startJuliaProcess(QStringList args)
 #endif
 
   QString juliaengine_path = Core::ICore::resourcePath() + QLatin1String("/Console");
-  args.append(juliaengine_path + "/Console.jl");
+  args.append( "-F" );
+  args.append( juliaengine_path + "/Console.jl");
 
   process->setWorkingDirectory(juliaengine_path);
   process->start( process_string, args, QProcess::ReadWrite );
